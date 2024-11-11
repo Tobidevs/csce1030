1ba9951bc6fe789fa84c619fe5be41e5cfd1f1f8
tobidevs <tobiakere05@gmail.com>
2024-11-11 14:10:26 -0600

    added add option

--- myproject1/myproject1.cpp
+++ myproject1/myproject1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <cctype>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main() {
     int points = 50;
-    enum choices {add, subtract, display, giveup, exit};
+    enum choices {add = 1, subtract, display, giveup, exit};
     string name;
 
     cout << "By: Tobi Akere" << endl;
@@ -14,19 +16,88 @@ int main() {
 
     // Get Name from user
     bool name_value = false;
-
     while (!name_value) {
-        cout << "What is your name: " << endl;
+        cout << "What is your name: ";
         getline(cin, name);
         name_value = true;
 
         for (char c : name) {
-            if (isalnum(c) || isspace(c)) {
+            if (!isalnum(c) && !isspace(c)) {
                 name_value = false;
                 cout << "Invalid name!" << endl;
             }
         }
     }
+
+    cout << "Welcome " << name << "!" << endl;
+
+
+    bool play_again = true;
+    while (play_again) {
+        // Generate random numbers
+        srand(time(0));
+        int num1 = 50 + rand() % 51;
+        int num2 = 50 + rand() % 51;
+
+        while(num1 >= num2) {
+            num1 = 50 + rand() % 51;
+            num2 = 50 + rand() % 51;
+        }
+        cout << "Your two random numbers have been generated." << endl;
+
+
+        // Menu choice
+        cout << "What would you like to do? Enter corresponding number." << endl;
+        cout << "1. ADD" << endl;
+        cout << "2. SUBTRACT" << endl;
+        cout << "3. DISPLAY" << endl;
+        cout << "4. GIVEUP" << endl;
+        cout << "5. EXIT" << endl;
+
+        char play_again_char;
+        bool play_again_value = true;
+        int choice;
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case add: 
+                int guessed_sum;
+                int sum_of_random = num1 + num2;
+                cout << "Guess the sum of the two numbers: ";
+                cin >> guessed_sum;
+
+                if (abs(sum_of_random - guessed_sum) < 5) {
+                    points += 5;
+                    cout << "Correct! You now have " << points << " points." << endl;
+                } else {
+                    points -= 5;
+                    cout << "Incorrect! You now have " << points << " points." << endl;
+                    
+                }
+                while (play_again_value) {
+                    cout << "Do you want to play agian? (y/n): ";
+                    cin >> play_again_char;
+                    if (play_again_char == 'y') {
+                        play_again = true;
+                        play_again_value = false;
+                    } else if (play_again_char == 'n') {
+                        play_again = false;
+                        play_again_value = false;
+                    } else {
+                        cout << "Invalid response!" << endl;
+                        play_again_value = true;
+                    }
+                }
+                
+                break;
+
+
+
+        }
+    }
+
+
 
     
 }
